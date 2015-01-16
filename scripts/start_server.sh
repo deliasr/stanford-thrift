971e54c2c85f1f6f89bc57d4d3cf50dde7a5825e
Matt Jackson <email@mwjackson.net>
2015-01-16 13:59:52 +0000

    package all deps into jar
    - updated startup script with single jar

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,14 @@
-#!/bin/zsh
+#!/bin/sh
 
-MAINDIR=$(dirname $0:A)/../
-CLASSDIR=$(dirname $0:A)/../target/classes/
+MAINJAR=$(dirname $0:A)/../target/stanford-corenlp-wrapper-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 if [ $# -eq 3 ]; then
  PORT=$1
  HEAPSIZE=$2
  CONFIG=$3
- java -cp $CLASSPATH:$MAINDIR/"*":$CLASSDIR -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit org.ets.research.nlp.stanford_thrift.StanfordCoreNLPServer $PORT $CONFIG
+ java -cp $MAINJAR -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit org.ets.research.nlp.stanford_thrift.StanfordCoreNLPServer $PORT $CONFIG
 # java -cp $CLASSPATH:$MAINDIR/"*":$MAINDIR/out-java/production/stanford
 # -thrift -Xmx$HEAPSIZE -XX:-UseGCOverheadLimit org.ets.research.nlp.stanford_thrift.StanfordCoreNLPServer $PORT $CONFIG
- echo $MAINDIR
 else
  echo "Usage: $(basename $0) <port> <heapsize> <config file>"
  echo "See scripts/ for an example config file."
