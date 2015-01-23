dd92e8aade9ba1686c1f498bb2c995b784e6aee7
Matt Jackson <email@mwjackson.net>
2015-01-23 14:37:22 +0000

    added sdist for python package
    added auto-incr version for package

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+with open('../../../version') as f:
+    version = f.readline()
+
+# WARNING: copy this to build/thrift/py before running setup.py
+setup(
+    name='knowsis.stanford-corenlp.thrift',
+    include_package_data=True,
+    version=version,
+    # package_dir={'': 'build/thrift/py'},
+    # packages=find_packages('build/thrift/py/corenlp'),
+    packages=[
+        'corenlp'
+    ],
+    description='Thrift client for Stanford CoreNLP service',
+    author='Knowsis Ltd',
+    author_email='dev@knows.is',
+    url="https://github.com/knowsis/stanford-thrift"
+)
